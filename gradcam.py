6d5ef249c2af19cb68651511b0176409fa689ee8
RidhwanD <ridhwan.dewoprabowo@gmail.com>
2023-12-01 06:52:04 +0700

    Add Score-CAM implementation

--- gradcam.py
+++ gradcam.py
@@ -111,11 +111,14 @@ image_size:tuple=None, figname:str=None, figsize:tuple=(3,3)):
 def Grad_CAM_plusplus(image:torch.Tensor, cluster, class_idx, model:SiameseNetwork=None, sub_network:int=None,\
 image_size:tuple=None, figname:str=None, figsize:tuple=(3,3)):
     '''
+        Adapted from https://github.com/1Konny/gradcam_plus_plus-pytorch
         Calculates the heatmap for Gram-CAM procedure
 
         Parameters
         ----------
         image: requested image
+        cluster: image cluster
+        class_idx: index of resulting class
         model: Similarity model (Siamese network)
         sub_network: requested sub_network 1 or 2
         image_size: image size
@@ -194,6 +197,90 @@ image_size:tuple=None, figname:str=None, figsize:tuple=(3,3)):
 
     return heatmap
 
+def Score_CAM(image: torch.Tensor, cluster, model: SiameseNetwork, target_class, device, image_size=None, figname=None, figsize=(3,3)):
+    '''
+    Generate a Score-CAM heatmap
+
+    Parameters
+    ----------
+    image: Input image tensor
+    model: The neural network model
+    target_class: Target class index
+    image_size: Size to resize the heatmap (optional)
+    figname: Filename to save the figure (optional)
+    figsize: Size of the figure (optional)
+
+    Returns
+    -------
+    heatmap
+    '''
+    # get the activations of the last convolutional layer
+    activations = model.get_activations(image).detach()
+
+    # Initialize scores
+    scores = torch.zeros(activations.shape[1], dtype=torch.float32)
+
+    # Get the size of the original image
+    original_size = image.shape[-2:]
+    
+    N = 10  # Number of top activation maps to consider
+    activations = activations[:, activations.mean(dim=(2, 3)).topk(N, dim=1).indices.squeeze(), :, :]
+    
+    # Iterate over each activation map
+    for i in range(activations.shape[1]):
+        print("Process ",i+1, " of ", activations.shape[1])
+        # Upsample activation to match the size of the original image
+        upsampled_activation = F.interpolate(activations[:, i:i+1, :, :], size=original_size, mode='bilinear', align_corners=False)
+
+        # Mask the input image with upsampled activation map
+        masked_input = image * upsampled_activation
+
+        # Forward pass with masked input
+        output = model(masked_input, cluster)
+
+        # Record the score for the target class
+        scores[i] = output[0, target_class]
+
+    # Normalize scores
+    scores = scores - scores.min()
+    scores = scores / scores.max()
+
+    scores = scores.to(device)
+
+    # Weight and combine activation maps
+    weighted_maps = activations * scores.view(-1, 1, 1, 1)
+    heatmap = weighted_maps.sum(0)
+
+    # Apply ReLU and normalize heatmap
+    heatmap = F.relu(heatmap)
+    heatmap /= heatmap.max()
+
+    # Post-processing steps similar to Grad-CAM.
+    
+    # Reshape & Convert Tensor to numpy
+    heatmap = torch.mean(heatmap, 0)
+    heatmap = heatmap.squeeze()
+    heatmap = heatmap.detach().cpu().numpy()
+
+
+    # Resize Heatmap
+    heatmap = cv2.resize(heatmap, image_size)
+    # Convert to [0,255]
+    heatmap = np.uint8(255 * heatmap)
+    
+    if figname is not None:
+        plt.figure(figsize=figsize);
+        fig = plt.imshow(heatmap);
+        fig.axes.get_xaxis().set_visible(False)
+        fig.axes.get_yaxis().set_visible(False)
+
+        plt.savefig(figname, dpi=300, format='png', 
+                bbox_inches='tight', pad_inches=0.1,
+                facecolor='auto', edgecolor='auto',
+                backend=None, 
+            )
+    
+    return heatmap
 
 def combineHeatmap(heatmaps, mode='max'):
     '''
